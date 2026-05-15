6cc4e1c06e60145c987fb27cd4bdd36e9ea7a9d3
Marcia Chen <marciachen.inpe@gmail.com>
2026-05-15 10:52:16 -0300

    Update tile parameter in mixture model script

--- scripts/1_mixture_model.R
+++ scripts/1_mixture_model.R
@@ -17,7 +17,7 @@ cube <- sits_cube(
   source     = "BDC",
   collection = "SENTINEL-2-16D",
   bands      = c('B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B11', 'B12', 'CLOUD'),
-  tiles      = '034010',
+  tiles      = '034011',
   start_date = '2024-08-01',
   end_date   = '2025-07-31',
   progress   = TRUE)
