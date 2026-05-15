ef4ca466c9f185ca2d28e469026ac4b84f6c33a7
MarciaChen1 <marciachen.inpe@gmail.com>
2026-05-15 13:37:02 +0000

    changed tile in mixture model

--- scripts/1_mixture_model.R
+++ scripts/1_mixture_model.R
@@ -17,7 +17,7 @@ cube <- sits_cube(
   source     = "BDC",
   collection = "SENTINEL-2-16D",
   bands      = c('B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B11', 'B12', 'CLOUD'),
-  tiles      = c('012014', '012015', '013014', '013015'),
+  tiles      = '034010',
   start_date = '2024-08-01',
   end_date   = '2025-07-31',
   progress   = TRUE)
